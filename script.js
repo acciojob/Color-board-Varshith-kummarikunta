e1a233dad8c1dcf47394cd56bed941a759d3e5cb
Varshith-kummarikunta <varshithkummarikunta@gmail.com>
2025-09-08 16:12:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const container = document.querySelector(".container")
+
+
+        for(let i = 1;i <= 800; i++){
+           createSquare()
+        }
+
+        function createSquare(){
+            const square = document.createElement("div")
+            square.classList.add("square");
+            square.addEventListener("mouseover",addRandomColor)
+            container.appendChild(square)
+        }
+
+        function addRandomColor(e){
+            const square = e.target
+            const randomColor = "#" + Math.floor(Math.random() *255 *255 *255).toString(16).padStart(6,"0")
+            console.log(randomColor,randomColor.length,'random coler')
+            square.style.backgroundColor = randomColor
+
+        square.addEventListener("mouseleave", () => { setTimeout(()=> e.target.style.backgroundColor = "",1000)
+        })
+    }
